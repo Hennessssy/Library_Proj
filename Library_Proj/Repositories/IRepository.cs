a6b93f45a750969c1b95355bfaadc9331a3a0da2
ajnur330 <mirsaitovajnur@gmail.com>
2025-12-19 10:56:46 +0300

    # Добавил репозитории

---
+++ Library_Proj/Repositories/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Library.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        // Получить все записи
+        IEnumerable<T> GetAll();
+
+        // Получить по ID
+        T GetById(int id);
+
+        // Найти по условию
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        // Добавить
+        void Add(T entity);
+
+        // Удалить
+        void Remove(T entity);
+
+        // Обновить
+        void Update(T entity);
+
+        // Сохранить изменения
+        int SaveChanges();
+    }
+}
---
+++ Library_Proj/Repositories/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Library_Proj.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Library.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly LibraryContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(LibraryContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public virtual T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+
+        public virtual void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
